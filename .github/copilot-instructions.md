98b0d184aed6fd634c9ae783cfc5fef35c7403b6
GASP3D4L <62368342+PIndividual@users.noreply.github.com>
2025-10-07 19:30:34 +1100

    Week 4
    Regular content push

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -1,8 +1,4 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: DDES9912_PeterQin
 - Unity version: Unity 6000.2.4f1
-- Active game object:
-  - Name: 2 Gear Rig - Compound 2 - Physics Enabled (2)
-  - Tag: Untagged
-  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/__DDES9912 Work/Scripts/Spin.cs
+++ Assets/__DDES9912 Work/Scripts/Spin.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Spin : MonoBehaviour
 {
     public float yspeed;
     public float xspeed;
     public float zspeed;
+    public float xacceleration;
+    public float zacceleration;
+    public float yacceleration;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,21 +17,56 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        xspeed += xacceleration * Time.deltaTime;
+        yspeed += yacceleration * Time.deltaTime;
+        zspeed += zacceleration * Time.deltaTime;
+    
         transform.Rotate(xspeed * Time.deltaTime, yspeed * Time.deltaTime, zspeed*Time.deltaTime);//Time.deltaTime的含义是每秒的时间，deltaTime代表每两帧之间的时间差，帧率越高数字越低，帧率越低数字越高，因此补全了不同系统之间帧率不同所带来的数字差
+    
     }
 
     public void Stop()
     {
-        yspeed = 0;
         xspeed = 0;
+        yspeed = 0;
         zspeed = 0;
+        xacceleration = 0;
+        yacceleration = 0;
+        zacceleration = 0;
     }
 
-    //public void setSpeed(float xnewSpeed float ynewSpeed float znewSpeed)
-    public void setSpeed(float ynewSpeed)
+    //public void setSpeed(float xnewSpeed, float ynewSpeed, float znewSpeed)
+    public void setSpeedY (float ynewSpeed)
     {
         //xspeed = xnewSpeed;
         yspeed = ynewSpeed;
         //zspeed = znewSpeed;
     }
+    public void setSpeedX(float xnewSpeed)
+    {
+        xspeed = xnewSpeed;
+        //yspeed = ynewSpeed;
+        //zspeed = znewSpeed;
+    }
+
+    public void setSpeedZ(float znewSpeed)
+    {
+        //xspeed = xnewSpeed;
+        //yspeed = ynewSpeed;
+        zspeed = znewSpeed;
+    }
+
+    public void setAccelerationX(float xnewAcceleration)
+    {
+        xacceleration = xnewAcceleration;
+    }
+    public void setAccelerationY(float ynewAcceleration)
+    {
+        yacceleration = ynewAcceleration;
+    }
+    public void setAccelerationZ(float znewAcceleration)
+    {
+        zacceleration = znewAcceleration;
+    }
+
 }
