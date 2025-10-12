3bf5e864223174c3c9ad778970565390fb6a8fe5
GASP3D4L <62368342+PIndividual@users.noreply.github.com>
2025-10-12 23:47:05 +1100

    Basic attempt on height adjustment
    there is still bugs

--- Assets/__DDES9912 Work/Scripts/Height_Adjust.cs
+++ Assets/__DDES9912 Work/Scripts/Height_Adjust.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+// This script is written based on the spin script
 public class Height_Adjust : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,6 +12,17 @@ public class Height_Adjust : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0,0,heightOffset * Time.deltaTime);
+        //To change the height of an item, we need to modify its y position based on the gizmo in my scene 
+        transform.Translate(0,heightOffset * Time.deltaTime,0);
+    }
+
+    public void setHeightOffset(float newHeightOffset)
+    {
+        heightOffset = newHeightOffset;
+    }
+    public void Stop()// If I don't put this here, there won't be a way to stop the change in height once the function is triggered by the button
+    {
+        heightOffset = 0;
+
     }
 }
