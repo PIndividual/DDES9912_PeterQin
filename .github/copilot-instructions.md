bde90dee288bdfe79aa21b0681861bad95e4c4ed
GASP3D4L <62368342+PIndividual@users.noreply.github.com>
2025-10-28 00:47:25 +1100

    Challenge task 2 cleared

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: DDES9912_PeterQin
 - Unity version: Unity 6000.2.4f1
 - Active game object:
-  - Name: Ghost Head Rig
+  - Name: Ghost Head Floating
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/__DDES9912 Work/Scripts/Chase_Around.cs
+++ Assets/__DDES9912 Work/Scripts/Chase_Around.cs
@@ -6,6 +6,7 @@ public class Chase_Around : MonoBehaviour
 {
     public NavMeshAgent AI;
     public Transform target;
+    public float activateDistance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +17,22 @@ public class Chase_Around : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AI.destination = target.position;
+
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        //AI.destination = target.position;
+        if (distanceToTarget < activateDistance)
+        {
+
+            AI.isStopped = false;
+            AI.destination = target.position;
+        }
+        else
+        {
+            
+            AI.isStopped = true;
+           
+        }
 
     }
 }
