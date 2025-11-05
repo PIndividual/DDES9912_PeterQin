79417bae09e2990b94b8375492eb8675c6a6f88f
GASP3D4L <62368342+PIndividual@users.noreply.github.com>
2025-11-06 00:07:20 +1100

    Basic NPC + Navmesh obstacles

--- Assets/__DDES9912 Work/Scripts/Wonder_Around.cs
+++ Assets/__DDES9912 Work/Scripts/Wonder_Around.cs
@@ -7,9 +7,9 @@ public class Wonder_Around : MonoBehaviour
 {
 
     public NavMeshAgent AI;
-    public float range; //创建新路径点距离自身当前位置的距离
-    public Transform centrePoint; //创建新路径点基于的中心位置，可以是某个参照物或者是自身，后者将会起到大范围移动的效果
-    public Vector3 newLocation;
+    public Transform nextCentrePoint; //创建新路径点基于的中心位置，可以是某个参照物或者是自身，后者将会起到大范围移动的效果
+    public float nextRange; //创建新路径点距离自身当前位置的距离
+    private Vector3 nextLocation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,12 +23,12 @@ public class Wonder_Around : MonoBehaviour
         if (AI.remainingDistance <= AI.stoppingDistance)
         {
             // 1. 直接在中心点周围的球体内生成一个随机点
-            newLocation = centrePoint.position + Random.insideUnitSphere * range;
+            nextLocation = nextCentrePoint.position + Random.insideUnitSphere * nextRange;//https://docs.unity3d.com/6000.2/Documentation/ScriptReference/Random-insideUnitSphere.html
 
-            AI.SetDestination(newLocation);
+            AI.SetDestination(nextLocation);
 
           
-           Debug.DrawRay(newLocation, Vector3.up, Color.blue, 1.0f);
+           //Debug.DrawRay(newLocation, Vector3.up, Color.blue, 1.0f);
         }
     }
 }
