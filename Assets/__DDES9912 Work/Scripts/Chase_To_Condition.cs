7020e4e789e51ec17978ba3fbf00b6bd17cd92a8
GASP3D4L <62368342+PIndividual@users.noreply.github.com>
2025-11-18 01:12:35 +1100

    Checkpoint
    Push button and walk away

--- Assets/__DDES9912 Work/Scripts/Chase_To_Condition.cs
+++ Assets/__DDES9912 Work/Scripts/Chase_To_Condition.cs
@@ -12,8 +12,11 @@ public class Chase_To_Condition : MonoBehaviour
     //public Transform target2;
     Animator Animate;//https://docs.unity3d.com/6000.2/Documentation/ScriptReference/Animator.SetFloat.html
     public UnityEvent actionOnTarget;
-    //public bool missionComplete;
-
+    public bool missionComplete;
+    //Ported from Wonder_Around
+    public Transform nextCentrePoint; //创建新路径点基于的中心位置，可以是某个参照物或者是自身，后者将会起到大范围移动的效果
+    public float nextRange; //创建新路径点距离自身当前位置的距离
+    private Vector3 nextLocation;
 
 
 
@@ -27,8 +30,8 @@ public class Chase_To_Condition : MonoBehaviour
         AI = GetComponent<NavMeshAgent>();
         Animate = gameObject.GetComponent<Animator>();
         Animate.SetFloat("ReachTarget", 0);
-        //Animate.SetBool("missionComplete", false);
-        //missionComplete = false;
+        Animate.SetBool("missionComplete", false);
+        missionComplete = false;
     }
 
     // Update is called once per frame
@@ -41,10 +44,17 @@ public class Chase_To_Condition : MonoBehaviour
             AI.destination = target1.position;
         }
         
-        if (distanceToTarget < 0.2) {
+        if (distanceToTarget < 0.2 && 0<distanceToTarget) {
             Animate.SetFloat("ReachTarget", 1.0f);
             actionOnTarget.Invoke();
-            //Animate.SetBool("missionComplete", true);
+            Animate.SetBool("missionComplete", true);
+            missionComplete = true;
+        }
+
+        if (missionComplete == true) {
+            nextLocation = nextCentrePoint.position + Random.insideUnitSphere * nextRange;//https://docs.unity3d.com/6000.2/Documentation/ScriptReference/Random-insideUnitSphere.html
+            activateDistance = 0;
+            AI.SetDestination(nextLocation);
         }
         //Debug.Log(distanceToTarget);
 
