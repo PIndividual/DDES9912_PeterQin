fdd17e49a797e2bd531649e3b8d0af5d3b842286
GASP3D4L <62368342+PIndividual@users.noreply.github.com>
2025-11-26 16:58:52 +1100

    Tips box implementation
    1st iteration

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: DDES9912_PeterQin
 - Unity version: Unity 6000.2.12f1
 - Active game object:
-  - Name: Come around (2)
+  - Name: Cube
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/__DDES9912 Work/Scripts/Countdown_function.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Countdown_function : MonoBehaviour
+{
+    public float countdownTime; // Total countdown time in seconds
+    private float timer;
+    public bool isActivated;
+    public UnityEvent onCountdownFinished;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //isActivated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActivated)
+        {
+            timer += Time.deltaTime;
+            if (timer >= countdownTime)
+            {
+                Debug.Log("Countdown finished!");
+                onCountdownFinished.Invoke();
+                timer = 0f; // Reset timer if you want to repeat the countdown
+                isActivated = false;
+            }
+        }
+        Debug.Log(timer);
+        Debug.Log(isActivated);
+    }
+     public void ActivateCountdown()
+    {
+        isActivated = true;
+        //timer = 0f; // Reset timer when activated
+    }
+}
